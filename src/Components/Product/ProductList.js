3d4043244bc58a459c59e7d13f999d26c58648b0
jhivesh subron <jhivsubron127@gmail.com>
2020-05-19 19:59:54 +0400

    Map() strapi api to productlist state

--- src/Components/Product/ProductList.js
+++ src/Components/Product/ProductList.js
@@ -1,77 +1,126 @@
 import React, { Component } from 'react'
 import ProductCard from "./ProductCard";
 import styled from "styled-components";
-import {storeProduct} from "./../../assets/Data";
+import { storeProduct } from "./../../assets/Data";
+import { Link } from "react-router-dom";
 
 export default class ProductList extends Component {
     state = {
-        product: storeProduct
+        loading: true,
+        products: []
+        // product: storeProduct
     }
-    render() {
-        console.log(this.state.product);
-        return (
-            <div className="container">
-                
-                {/* row1 */}
-                <div class="row">
-                    <Heading >Our Products</Heading>
-                </div>
+    async componentDidMount() {
+        let response = await fetch("http://localhost:1337/product")
+        if (!response.ok){
+            return
+        }
+        let products = await response.json()
+        this.setState({
+            loading:false,
+            products : products
+        })
+    }
+render() {
+   
+    if (!this.state.loading) {
+    return (
+        <div className="container">
+            <h2>Available Products : {this.state.products.length}</h2>
+            <img src={this.state.products.image} alt=""/>
+            {this.state.products.map((product,index)=>{
+                return(
+                    <div className="container">
+                        <div className="row">
+                        <Link to={`/product/${product.id}`}>
+                            <ProductCard title={product.title} price={product.price} img={product.img} />
+                        </Link>
+                        </div>
+                        
+                    </div>
+                )
+            })}
 
-                <div class="row">
-                    <div class="col">
-                        <ProductCard title={this.state.product[0].title} price={this.state.product[0].price} img={this.state.product[0].img}/>
-                    </div>
-
-                    <div class="col">
-                        <ProductCard title={this.state.product[1].title} price={this.state.product[1].price} img={this.state.product[1].img}/>
-                    </div>
-                    <div class="col">
-                        <ProductCard title={this.state.product[2].title} price={this.state.product[2].price} img={this.state.product[2].img} />
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col">
-                        <ProductCard title={this.state.product[3].title} price={this.state.product[3].price} img={this.state.product[3].img}/>
-                    </div>
-
-                    <div class="col">
-                        <ProductCard title={this.state.product[4].title} price={this.state.product[4].price} img={this.state.product[4].img}/>
-                    </div>
-                    <div class="col">
-                        <ProductCard title={this.state.product[5].title} price={this.state.product[5].price} img={this.state.product[5].img}/>
-                    </div>
-                </div>
+        </div>
 
 
-                <div class="row">
-                    <div class="col">
-                        <ProductCard title={this.state.product[6].title} price={this.state.product[6].price} img={this.state.product[6].img}/>
-                    </div>
 
-                    <div class="col">
-                        <ProductCard title={this.state.product[7].title} price={this.state.product[7].price} img={this.state.product[7].img}/>
-                    </div>
-                    <div class="col">
-                        <ProductCard />
-                    </div>
-                </div>
 
-                <div class="row">
-                    <div class="col">
-                        <ProductCard />
-                    </div>
 
-                    <div class="col">
-                        <ProductCard />
-                    </div>
-                    <div class="col">
-                        <ProductCard />
-                    </div>
-                </div>
-                
-            </div>
-        )
+
+
+
+
+
+
+
+
+
+                // <div classNameName="container">
+
+                //     {/* row1 */}
+                //     <div className="row">
+                //         <Heading >Our Products</Heading>
+                //     </div>
+
+                //     <div className="row">
+                //         <div className="col">
+                //             <ProductCard />
+                //         </div>
+
+                //         <div className="col">
+                //             <ProductCard />
+                //         </div>
+                //         <div className="col">
+                //             <ProductCard />
+                //         </div>
+                //     </div>
+
+                //     <div className="row">
+                //         <div className="col">
+                //             <ProductCard  />
+                //         </div>
+
+                //         <div className="col">
+                //             <ProductCard  />
+                //         </div>
+                //         <div className="col">
+                //             <ProductCard/>
+                //         </div>
+                //     </div>
+
+
+                //     <div className="row">
+                //         <div className="col">
+                //             <ProductCard />
+                //         </div>
+
+                //         <div className="col">
+                //             <ProductCard />
+                //         </div>
+                //         <div className="col">
+                //             <ProductCard />
+                //         </div>
+                //     </div>
+
+                //     <div className="row">
+                //         <div className="col">
+                //             <ProductCard />
+                //         </div>
+
+                //         <div className="col">
+                //             <ProductCard />
+                //         </div>
+                //         <div className="col">
+                //             <ProductCard />
+                //         </div>
+                //     </div>
+
+                // </div>
+            );
+        }
+        return (<h2>Loading : Waiting for API</h2>)
+
     }
 }
 const Heading = styled.h4`
