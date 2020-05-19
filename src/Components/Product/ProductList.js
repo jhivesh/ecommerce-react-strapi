ea7feaff714f1b0ebafc47926cbc8f0aaa569702
jhivesh subron <jhivsubron127@gmail.com>
2020-05-19 20:05:46 +0400

    map() product list

--- src/Components/Product/ProductList.js
+++ src/Components/Product/ProductList.js
@@ -12,36 +12,39 @@ export default class ProductList extends Component {
     }
     async componentDidMount() {
         let response = await fetch("http://localhost:1337/product")
-        if (!response.ok){
+        if (!response.ok) {
             return
         }
         let products = await response.json()
         this.setState({
-            loading:false,
-            products : products
+            loading: false,
+            products: products
         })
     }
-render() {
-   
-    if (!this.state.loading) {
-    return (
-        <div className="container">
-            <h2>Available Products : {this.state.products.length}</h2>
-            <img src={this.state.products.image} alt=""/>
-            {this.state.products.map((product,index)=>{
-                return(
-                    <div className="container">
-                        <div className="row">
-                        <Link to={`/product/${product.id}`}>
-                            <ProductCard title={product.title} price={product.price} img={product.img} />
-                        </Link>
-                        </div>
-                        
-                    </div>
-                )
-            })}
+    render() {
 
-        </div>
+        if (!this.state.loading) {
+            return (
+                <div className="container">
+                    <h2>Available Products : {this.state.products.length}</h2>
+                    <img src={this.state.products.image} alt="" />
+                    {this.state.products.map((product, index) => {
+                        return (
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col">
+                                        <Link to={`/product/${product.id}`}>
+                                            <ProductCard title={product.title} price={product.price} img={product.img} />
+                                        </Link>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        )
+                    })}
+
+                </div>
 
 
 
