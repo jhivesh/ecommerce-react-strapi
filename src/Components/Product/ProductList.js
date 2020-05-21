40137871299e30463e8bc24fb9b85e59a176440e
jhivesh subron <jhivsubron127@gmail.com>
2020-05-21 12:29:53 +0400

    productlist with bootstrap

--- src/Components/Product/ProductList.js
+++ src/Components/Product/ProductList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import ProductCard from "./ProductCard";
 import styled from "styled-components";
-import { storeProduct } from "./../../assets/Data";
 import { Link } from "react-router-dom";
+//--------------------------------------------------------
 
 export default class ProductList extends Component {
     state = {
@@ -21,112 +20,125 @@ export default class ProductList extends Component {
             products: products
         })
     }
+
+    // handlecart = (e) =>{
+    //  //<Link to={`/product/${product.id}`}>  </Link>
+    //     console.log(this.element);
+
+
+    //  }
+
+
     render() {
+
+        const handlecart = (product) => {
+            console.log(product.id);
+        }
 
         if (!this.state.loading) {
             return (
-                <div className="container">
-                    <h2>Available Products : {this.state.products.length}</h2>
-                    <img src={this.state.products.image} alt="" />
-                    {this.state.products.map((product, index) => {
-                        return (
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col">
-                                        <Link to={`/product/${product.id}`}>
-                                            <ProductCard title={product.title} price={product.price} img={product.img} />
-                                        </Link>
+
+        <div className="container">
+            {/* heading */}
+            <h2>Available Products : {this.state.products.length}</h2>
+
+            {this.state.products.map((product, index) => {
+                return (
+
+        
+                        <div className="row">
+                            <div className="col">
+
+                                {/* Product card */}
+                                <Card className="card">
+
+                                    <div className="card-body">
+
+                                        <Title className="card-title">{product.title}</Title>
+
+                                        <img src={product.img} alt="Image" height="50" width="500" />
+
+                                        <Description className="card-text"> Price: Rs {product.price}</Description>
+
+                                        <ActionButton onClick={() => { handlecart(product) }}>Add to Cart </ActionButton>
+
                                     </div>
 
-                                </div>
+                                </Card>
 
                             </div>
-                        )
-                    })}
 
-                </div>
+                        </div>
+                    
+                );
+            })};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-                // <div classNameName="container">
-
-                //     {/* row1 */}
-                //     <div className="row">
-                //         <Heading >Our Products</Heading>
-                //     </div>
-
-                //     <div className="row">
-                //         <div className="col">
-                //             <ProductCard />
-                //         </div>
-
-                //         <div className="col">
-                //             <ProductCard />
-                //         </div>
-                //         <div className="col">
-                //             <ProductCard />
-                //         </div>
-                //     </div>
-
-                //     <div className="row">
-                //         <div className="col">
-                //             <ProductCard  />
-                //         </div>
-
-                //         <div className="col">
-                //             <ProductCard  />
-                //         </div>
-                //         <div className="col">
-                //             <ProductCard/>
-                //         </div>
-                //     </div>
-
-
-                //     <div className="row">
-                //         <div className="col">
-                //             <ProductCard />
-                //         </div>
-
-                //         <div className="col">
-                //             <ProductCard />
-                //         </div>
-                //         <div className="col">
-                //             <ProductCard />
-                //         </div>
-                //     </div>
-
-                //     <div className="row">
-                //         <div className="col">
-                //             <ProductCard />
-                //         </div>
-
-                //         <div className="col">
-                //             <ProductCard />
-                //         </div>
-                //         <div className="col">
-                //             <ProductCard />
-                //         </div>
-                //     </div>
-
-                // </div>
-            );
+        </div>
+            ); // END OF IF STATEMENT 
         }
-        return (<h2>Loading : Waiting for API</h2>)
+        return (<h2 className="container">Loading : Waiting for API</h2>)
 
     }
 }
 const Heading = styled.h4`
 align-items:center;
 padding-right:20rem;
+`
+const Card = styled.div`
+width: 18rem;
+margin-top:3rem;
+margin-right:1rem;
+border:none;
+
+:hover {
+    border:2px solid #e5e5e5;
+    background: lightblue;
+    transition: all 0.2s ease-out 0s;
+    display: block;
+    object-fit: cover;
+    border-color: rgb(229, 235, 237);
+  }
+  
+color: #333;
+  display: flex;
+  align-items: center;
+  svg {
+    transform: translateY(2px);
+    margin-right: 5px;
+  }
+  @media (max-width: 500px) {
+    flex-direction: column;
+    & button {
+      width: 100%;
+      margin-bottom: 4px;
+      font-size: 0.65rem;
+    }
+  }
+`
+//------------------------------------------------------------------
+
+
+//-----------Title CSS------------------------------------------------
+const Title = styled.h2`
+  color: 000000;
+  font-weight: 300;
+`
+//------------------------------------------------------------------
+
+const Description = styled.p`
+  color: 000000;
+  font-weight: 500;
+`
+const ActionButton = styled.button`
+  margin: 0 5px;
+  padding: 8px 14px;
+  background: lightblue;
+  color: blue;
+  cursor: pointer;
+  border: 1px solid #fff;
+  outline: 0;
+  font-weight: 300;
+  :hover {
+    opacity: 0.8;
+  }
 `
